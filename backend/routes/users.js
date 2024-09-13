1b55766fab2a9d9ca408b0c9820341dd2ef4e9f0
ameo <iamthief1412@gmail.com>
2024-09-13 17:33:01 +0700

    ..

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
 
 var modelUser = require('../models/userModel');
 
@@ -7,7 +8,7 @@ var modelUser = require('../models/userModel');
 router.post('/login', async function(req, res, next) {
   try{
     var {name, password } = req.body
-    var data = await modelUser.findOne({name, password});
+    var data = await modelUser.findOne({name});
     // res.render('admin_product', { title: 'Trang Quản Lý Sản Phẩm', products:data });
   }catch(e){
         res.json({status: 0, message:"không tìm thấy sản phẩm "})
