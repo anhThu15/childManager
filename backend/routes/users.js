171321b9b6b0c1000e7b4abae806880e8a779d4d
ameo <iamthief1412@gmail.com>
2024-09-13 17:29:10 +0700

    ,,

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var modelUser = require('../models/userModel');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', async function(req, res, next) {
+  try{
+    var {name, password } = req.body
+    var data = await modelUser.findOne({name, password});
+    // res.render('admin_product', { title: 'Trang Quản Lý Sản Phẩm', products:data });
+  }catch(e){
+        res.json({status: 0, message:"không tìm thấy sản phẩm "})
+  }
+  res.json(data)
 });
 
 module.exports = router;
