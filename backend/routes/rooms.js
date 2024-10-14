0ad04d57ad4d066407806c0391e3795db5c38bb6
ameo <iamthief1412@gmail.com>
2024-10-14 16:30:12 +0700

    new comit room

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -5,9 +5,21 @@ var modelRoom = require('../models/roomModel');
 var modelUser = require('../models/userModel');
 
 
-router.get('/', async function(req, res, next) {
-  const data = await modelRoom.find()
-  res.json(data)
+router.get('/:id_parish', async function(req, res, next) {
+  try{
+    const {id_parish} = req.params
+    const result = await modelRoom.find({id_parish:id_parish})
+
+
+    if(result != null){
+      res.json({status: 1, message:"Thành công", result});
+    }else{
+      res.json({status: 0, message:"thất bại"});
+    }
+
+  }catch(e){
+        res.json({status: 0, message:"không tìm thấy sản phẩm "})
+  }
 })
 
 
--- frontend/src/app/componets/listRoom.jsx
+++ frontend/src/app/componets/listRoom.jsx
@@ -1,9 +1,11 @@
 'use client'
 import { Accordion, AccordionContent, AccordionPanel, AccordionTitle,Button, Label, TextInput, Select, Datepicker, FileInput } from "flowbite-react";
 
+
 export default function ListRoom(){
     return (
     <div>
+      
         <Accordion style={{width: 239}} >
           <AccordionPanel>
             <AccordionTitle>Lớp Thêm Sức 1</AccordionTitle>
@@ -16,73 +18,8 @@ export default function ListRoom(){
               </p>             
             </AccordionContent>
           </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Thêm Sức 2</AccordionTitle>
-            <AccordionContent className="bg-slate-200">
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Thêm Sức 3</AccordionTitle>
-            <AccordionContent className="bg-slate-200">
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Xưng Tội 4</AccordionTitle>
-            <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Thêm Sức 5</AccordionTitle>
-            <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Thêm Sức 6</AccordionTitle>
-            <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>Lớp Thêm Sức 7</AccordionTitle>
-            <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Sỉ Số : 100
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                GLV: Thư
-              </p>      
-            </AccordionContent>
-          </AccordionPanel>
         </Accordion>
+
         <button className="btn w-full" onClick={()=>document.getElementById('my_modal_2').showModal()}><i class="fa-solid fa-plus"></i></button>
               <dialog id="my_modal_2" className="modal">
                 <div className="modal-box">
